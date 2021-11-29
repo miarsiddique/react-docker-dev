438c9ff33bd2af3b859a8372a94e459d5adaab85
irshad-infino <irshad@infino>
2021-11-29 14:22:47 +0530

    docker setup

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:14-alpine
+
+ENV NODE_VERSION 14.18.1
+ENV PATH /app/node_modules/.bin:$PATH
+
+WORKDIR '/app'
+
+COPY package.json .
+
+RUN npm install
+RUN npm install react-scripts@4.0.3 -g
+
+COPY . .
+
+CMD ["npm", "start"]
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Learn PWA / Magento Update Update </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
